4be5b5efd0f8288ce7c859ca6755e91dcd8340df
unknown <jhonirincon.700@hotmail.com>
2021-09-11 15:40:22 -0500

    se agrega servivivicios y sobre nosotros y hasta hay funciona

---
+++ src/app/components/froms/from-home/from-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-from-home',
+  templateUrl: './from-home.component.html',
+  styleUrls: ['./from-home.component.css']
+})
+export class FromHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
